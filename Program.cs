306242d8542eb843edc1f47bd06b0538977453a9
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-23 22:50:29 +0000

    Jan 23, 2024, 10:50 PM

--- Program.cs
+++ Program.cs
@@ -1,99 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
-
-
-[Flags]
-public enum SongGenre
+﻿namespace Week_01_lab_02_Cars_W
 {
-    Unclassified = 0,
-    Pop = 0b1,
-    Rock = 0b10,
-    Blues = 0b100,
-    Country = 0b1_000,
-    Metal = 0b10_000,
-    Soul = 0b100_000
-}
-
-public class Song
-{
-    public string Artist { get; }
-    public string Title { get; }
-    public double Length { get; }
-    public SongGenre Genre { get; }
-
-    public Song(string title, string artist, double length, SongGenre genre)
+    internal class Program
     {
-        Title = title;
-        Artist = artist;
-        Length = length;
-        Genre = genre;
-    }
-
-    public override string ToString()
-    {
-        return $"{Title} by {Artist} ({Genre}) {Length}min";
-    }
-}
-
-public static class Library
-{
-    private static List<Song> songs = new List<Song>();
-
-    public static void LoadSongs(string fileName)
-    {
-        using (StreamReader reader = new StreamReader(fileName))
+        static void Main(string[] args)
         {
-            string title;
-            while ((title = reader.ReadLine()) != null)
+            // To test the constructor and the ToString method
+
+            Console.WriteLine(new Song("Baby", "Justin Bebier", 3.35, SongGenre.Pop));
+
+            // This is first time that you are using the bitwise or. It is used to specify a combination of genres
+
+            Console.WriteLine(new Song("The Promise", "Chris Cornell", 4.26, SongGenre.Country | SongGenre.Rock));
+
+            // Class methods are invoke with the class name
+            Library.LoadSongs("Week_03_lab_09_songs4.txt"); 
+
+            Console.WriteLine("\n\nAll songs");
+
+            Library.DisplaySongs();
+
+            SongGenre genre = SongGenre.Rock;
+
+            Console.WriteLine($"\n\n{genre} songs");
+
+            Library.DisplaySongs(genre);
+
+            string artist = "Bob Dylan";
+
+            Console.WriteLine($"\n\nSongs by {artist}");
+
+            Library.DisplaySongs(artist);
+
+            double length = 5.0;
+
+            Console.WriteLine($"\n\nSongs more than {length}mins");
+
+            Library.DisplaySongs(length);
+        }
+    }
+
+
+    [Flags]
+    public enum SongGenre
+    {
+        Unclassified = 0,
+        Pop = 0b1,
+        Rock = 0b10,
+        Blues = 0b100,
+        Country = 0b1_000,
+        Metal = 0b10_000,
+        Soul = 0b100_000
+    }
+
+    public class Song
+    {
+        public string Artist { get; }
+        public string Title { get; }
+        public double Length { get; }
+        public SongGenre Genre { get; }
+
+        public Song(string title, string artist, double length, SongGenre genre)
+        {
+            Title = title;
+            Artist = artist;
+            Length = length;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Artist} ({Genre}) {Length}min";
+        }
+    }
+
+    public static class Library
+    {
+        private static List<Song> songs = new List<Song>();
+
+        public static void LoadSongs(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
             {
-                string artist = reader.ReadLine();
-                double length = Convert.ToDouble(reader.ReadLine());
-                SongGenre genre = (SongGenre)Enum.Parse(typeof(SongGenre), reader.ReadLine());
-                songs.Add(new Song(title, artist, length, genre));
+                string title;
+                while ((title = reader.ReadLine()) != null)
+                {
+                    string artist = reader.ReadLine();
+                    double length = Convert.ToDouble(reader.ReadLine());
+                    SongGenre genre = (SongGenre)Enum.Parse(typeof(SongGenre), reader.ReadLine());
+                    songs.Add(new Song(title, artist, length, genre));
+                }
             }
         }
-    }
 
-    public static void DisplaySongs()
-    {
-        foreach (var song in songs)
+        public static void DisplaySongs()
         {
-            Console.WriteLine(song);
-        }
-    }
-
-    public static void DisplaySongs(double longerThan)
-    {
-        foreach (var song in songs)
-        {
-            if (song.Length > longerThan)
+            foreach (var song in songs)
             {
                 Console.WriteLine(song);
             }
         }
-    }
 
-    public static void DisplaySongs(SongGenre genre)
-    {
-        foreach (var song in songs)
+        public static void DisplaySongs(double longerThan)
         {
-            if (song.Genre.HasFlag(genre))
+            foreach (var song in songs)
             {
-                Console.WriteLine(song);
+                if (song.Length > longerThan)
+                {
+                    Console.WriteLine(song);
+                }
             }
         }
-    }
 
-    public static void DisplaySongs(string artist)
-    {
-        foreach (var song in songs)
+        public static void DisplaySongs(SongGenre genre)
         {
-            if (song.Artist == artist)
+            foreach (var song in songs)
             {
-                Console.WriteLine(song);
+                if (song.Genre.HasFlag(genre))
+                {
+                    Console.WriteLine(song);
+                }
+            }
+        }
+
+        public static void DisplaySongs(string artist)
+        {
+            foreach (var song in songs)
+            {
+                if (song.Artist == artist)
+                {
+                    Console.WriteLine(song);
+                }
             }
         }
     }
 }
+
+
